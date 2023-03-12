86d7e4dee2bec1ea60dca2098e04748d53110035
valuvilla <valuvp@hotmail.com>
2023-03-12 16:24:57 +0100

    cambios

--- database.py
+++ database.py
@@ -21,86 +21,69 @@ class Polinomio(object):
         self.termino_mayor = None
         self.grado = -1
     
+    class datoPolinomio(object):
+    """Clase dato polinomio"""
+
+    def __init__(self, valor, termino):
+        """Crea un dato polinomio con valor y término"""
+        self.valor = valor
+        self.termino = termino
+
+
+class Polinomio(object):
+    """Clase polinomio"""
+
+    def __init__(self):
+        """Crea un polinomio del grado cero"""
+        self.termino_mayor = None
+        self.grado = -1
+
     def agregar_termino(polinomio, termino, valor):
-        """Agrega un termino y su valor al Polinomio. """
-        polinomio=Polinomio()
+        """Agrega un termino y su valor al polinomio"""
         aux = Nodo()
         dato = datoPolinomio(valor, termino)
         aux.info = dato
-    
-
-        # comparo el termino con el grado del polinomio
-        if(termino > polinomio.grado):
-            # si es mayor, lo agrego al principio
+        if (termino > polinomio.grado):
             aux.sig = polinomio.termino_mayor
             polinomio.termino_mayor = aux
             polinomio.grado = termino
-
         else:
-            actual=polinomio.termino_mayor
-            while(actual.sig is not None and actual.sig.info.termino > termino):
-                actual=actual.sig
-            aux.sig=actual.sig
-            actual.sig=aux
-
-    
-    def eliminar_termino(polinomio, termino):
-        """Elimina un termino del Polinomio."""
-        aux = Polinomio.termino_mayor
-        if(aux is not None):
-            if(aux.info.termino == termino):
-                Polinomio.termino_mayor = aux.sig
-                del aux
-            else:
-                while(aux.sig is not None and aux.sig.info.termino != termino):
-                    aux = aux.sig
-                if(aux.sig is not None):
-                    aux.sig = aux.sig.sig
-                    del aux.sig
-
-    def determinar_si_existee_termino(polinomio, termino):
-        """Determina si existe un termino en el Polinomio."""
-        # consultar si el resultado es distinto de cero para determinar si el polinomio tiene ese término o no
-        polinomio=Polinomio()
-        aux = polinomio.termino_mayor
-        while(aux is not None and aux.info.termino != termino):
-            aux = aux.sig
-        if(aux is not None):
-            return True
-        return False
-        
+            actual = polinomio.termino_mayor
+            while (actual.sig is not None and termino < actual.sig.info.termino):
+                actual = actual.sig
+            aux.sig = actual.sig
+            actual.sig = aux
 
     def modificar_termino(polinomio, termino, valor):
-        """Modifica el valor de un termino del Polinomio."""
-        polinomio=Polinomio()
+        """Modifica un termino del polinomio"""
         aux = polinomio.termino_mayor
-        while(aux is not None and aux.info.termino != termino):
+        while (aux is not None and aux.info.termino != termino):
             aux = aux.sig
         aux.info.valor = valor
 
-    def obtener_valor (polinomio, termino):
-        """Devuelve el valor de un termino del Polinomio."""
-        aux = Polinomio.termino_mayor
-        while(aux is not None and aux. info.termino > termino):
+    def obtener_valor(polinomio, termino):
+        """Devuelve el valor de un termino del polinomio"""
+        aux = polinomio.termino_mayor
+        while (aux is not None and aux.info.termino != termino):
             aux = aux.sig
-        if(aux is not None and aux.info.termino == termino): 
-            return aux.info.valor 
+        if (aux is not None and aux.info.termino == termino):
+            return aux.info.valor
         else:
             return 0
-
-    def mostrar (polinomio):
-        """Muestra el Polinomio."""
-        polinomio=Polinomio()
+        
+    def mostrar(polinomio):
+        """Muestra el polinomio"""
         aux = polinomio.termino_mayor
-        pol = ''
+        pol = ""
         if (aux is not None):
-            while(aux is not None):
-                signo = '' 
-                if(aux. info.valor > 0):
-                    signo += '+'
-                pol += signo + str(aux.info.valor)+"x^"+str(aux.info.termino)
+            while (aux is not None):
+                signo = ""
+                if aux.info.valor >= 0:
+                    signo += "+"
+                pol += signo + str(aux.info.valor) + "x^", str(aux.info.termino)
                 aux = aux.sig
         return pol
+    
 
     def sumar(polinomio1, polinomio2):
         """Suma dos polinomios y devuelve el resultado."""
